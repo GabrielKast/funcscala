145c65bbe6b35de2b535ee21182278d6de690f43
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2013-05-07 00:30:48 +0200

    chapter 3 exercice 1-5 + Added a errors.txt where I log some mistakes inthe exercices

---
+++ src/main/scala/chapter3.scala
@@ -0,0 +1,51 @@
+/* ============================================= */
+
+package fpinscala
+
+object chapter3 {
+  import fpinscala.datastructures._
+  //EXERCISE 1: What will the result of the following match expression be?
+  def ex1() = 
+    List(1,2,3,4,5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+      case _ => 101
+    }
+
+  // EXERCISE 2: Implement the function tail for "removing" the first element
+  // of a List. Notice the function takes constant time. What are different
+  // choices you could make in your implementation if the List is Nil? 
+  // We will return to this question in the next chapter.
+  def tail[A](l: List[A]): List[A]=l match {
+    case Nil => throw new UnsupportedOperationException
+    case Cons(_, rest) => rest
+  }
+
+  // EXERCISE 3: Generalize tail to the function drop, 
+  // which removes the first n elements from a list.
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n==0) l
+    else drop(tail(l), n-1)
+  }
+
+
+  // EXERCISE 4: Implement dropWhile, which removes elements from the
+  // List prefix as long as they match a predicate. Again, notice 
+  // these functions take time proportional only to the number of elements 
+  // being dropped—we do not need to make a copy of the entire List.
+
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(a, rest) if f(a) => dropWhile(rest)(f)
+    case _ => l
+  }
+
+  // EXERCISE 5: Using the same idea, implement the function setHead for
+  // replacing the first element of a List with a different value.
+  def setHead[A](l: List[A], a: A): List[A]= l match {
+    case Nil => throw new UnsupportedOperationException
+    case Cons(_, tail) => Cons(a, tail)
+  }
+}
---
+++ src/test/scala/chapter3Spec.scala
@@ -0,0 +1,67 @@
+import org.scalatest.FunSpec
+
+class Chapter3Spec extends FunSpec {
+  import fpinscala.datastructures._
+  import fpinscala.chapter3._
+
+  describe ("ex1 is an exercice about pattern matching") {
+    it ("should be 1+2=3") {
+      assert(ex1()===3)
+    }
+  }
+
+  describe ("ex2 : implement a tail") {
+    it ("should be List(2, 3)") {
+      assert(tail(List(1, 2, 3))===List(2, 3))
+    }
+    it ("should be Nil") {
+      assert(tail(List(1))===Nil)
+    }
+    it("Should throw an exception") {
+      intercept[UnsupportedOperationException] {
+	tail(Nil)
+      }
+    }
+  }
+
+  describe ("ex3 : implement a drop") {
+    it ("should remove 3 first elements") {
+      assert(drop(List(1, 2, 3, 4, 5), 3)===List(4, 5))
+    }
+    it ("should remove all  elements") {
+      assert(drop(List(1, 2, 3, 4, 5), 5)===Nil)
+    }
+    it ("should throw an exception") {
+      intercept[UnsupportedOperationException] {
+	drop(List(1, 2, 3, 4, 5), 6)
+      }
+    }
+  }
+
+  describe ("ex4 : implement a dropWhile") {
+    it ("should remove 3 first elements") {
+      assert(dropWhile(List(1, 2, 3, 4, 5)) (_<4) ===List(4, 5))
+    }
+    it ("should remove all  elements") {
+      assert(dropWhile(List(1, 2, 3, 4, 5))(_<10)===Nil)
+    }
+    it ("should keep all the elements") {
+      assert(dropWhile(List(1, 2, 3, 4, 5))(_<0)===List(1, 2, 3, 4, 5))
+    }
+    it ("should keep the Nil ") {
+      assert(dropWhile(Nil)(x => true)===Nil)
+    }
+  }
+
+  describe("Exercice 5: setHead") {
+    it("should replace the first element with a 99") {
+      assert(setHead(List(1, 2, 3, 4), 99)===List(99, 2, 3, 4))
+      assert(setHead(List(1), 99)===List(99))
+    }
+    it("should not work as there is no head : exception") {
+      intercept[UnsupportedOperationException] {
+	setHead(Nil, 99)
+      }
+    }
+  }
+}
