465e0482df968c0e9b18456a3a2abb0efd016b90
Gabriel Kastenbaum <gabriel.kastenbaum@gmail.com>
2013-05-19 00:18:58 +0200

    change filename

---
+++ src/main/scala/chapter2.scala
@@ -0,0 +1,55 @@
+package funcscala
+
+object chapter2 {
+  //EXERCISE 1 (optional): Write a function to get the nth Fibonacci number. 
+  // The first two Fibonacci numbers are 0 and 1,
+  // and the next number is always the sum of
+  // the previous two. 
+  // Your definition should use a local tail-recursive function.4 
+  def fibo(nth: Int):Int={
+    def fibo_acc(n:Int, n_plus_1:Int, i: Int):Int= {
+      if (i==nth) n
+      else fibo_acc(n_plus_1, n + n_plus_1, i+1)
+    }
+    fibo_acc(0, 1, 1)
+  }
+
+  // EXERCISE 2: Implement isSorted, which checks whether an Array[A] 
+  // is sorted according to a given comparison function.
+  def isSorted[A](as: Array[A], sorted: (A, A)=> Boolean): Boolean={
+    if (as.length<2) true
+    else (1 until as.length).forall(i=>sorted(as(i-1), as(i)))
+  }
+
+  // def partial1[A,B,C](a: A, f: (A,B) => C): B => C
+  // EXERCISE 3 (hard): Implement partial1 and write down a concrete usage
+  // of it. There is only one possible implementation that compiles. 
+  // We don't have any concrete types here, so we can only stick things
+  // together using the local 'rules of the universe' established by the type
+  // signature.
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = {
+    (b: B)=> f(a, b)
+  }
+
+  // EXERCISE 4 (hard): Let's look at another example, currying, 
+  // which converts a function of N arguments into a function of one argument
+  // that returns another function as its result.
+  // Here again, there is only one implementation that typechecks.
+  // def curry[A,B,C](f: (A, B) => C): A => (B => C)
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = 
+    (a: A) => ((b: B) => f(a, b))
+
+    
+  // Exercice 5 (optional): Implement uncurry, which reverses 
+  // the transformation of curry. 
+  // Note that since => associates to the right, 
+  // A => (B => C) can be written as A => B => C.
+  // def uncurry[A,B,C](f: A => B => C): (A, B) => C
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = 
+      (a: A, b: B) => f(a)(b)
+
+  // EXERCISE 6: Implement the higher-order function 
+  // that composes two functions.
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+}
+
---
+++ src/test/scala/chapter2Spec.scala
@@ -0,0 +1,80 @@
+import org.scalatest.FunSpec
+
+class Chapter2Spec extends FunSpec {
+  import funcscala.chapter2._
+
+  describe("Exercice 1: tailrec version of Fibo ") {
+    it("should give 0 for the first result") {
+      assert(fibo(1)===0)
+    }
+    it("should give 1 for the second result") {
+      assert(fibo(2)===1)
+    }
+    it("should give 35 for the 10nth result") {
+      assert(fibo(2)===1)
+    }
+    it("should give the first 10 fibo numbers") {
+      val l=(1 to 10).map(fibo(_))
+      assert(l === List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+    }
+  }
+
+  describe("Exercice 2 : is_sorted?") {
+    val int_sorter = (a: Int,b: Int)=> a<b
+    val string_sorter = (a: String,b: String)=> a<b
+    it("should be sorted with ints") {
+      assert(isSorted(Array(1, 2, 3, 4), int_sorter)===true)
+    }
+    it("should not be sorted simple") {
+      assert(isSorted(Array(1, 2, 1, 3, 4), int_sorter)===false)
+    }
+    it("should not be sorted - not sorted at the end of the array") {
+      assert(isSorted(Array(1, 2, 3, 4, 1), int_sorter)===false)
+    }
+    it("should not be sorted - not sorted at the beginning of the array") {
+      assert(isSorted(Array(2, 1, 2, 3, 4), int_sorter)===false)
+    }
+    it("should be sorted with strings") {
+      assert(isSorted(Array("la", "mi", "re", "sol"), string_sorter)===true)
+    }
+  }
+  
+  describe("Exercice 3 : Partial1") {
+    it ("should give a funtion that adds 1") {
+      val result=partial1(1, (a: Int, b:Int)=> a+b)
+      assert(result(2)===3)
+      assert(result(3)===4)
+    }
+    it ("should give a funtion that gives string.length * Int /10 ") {
+      val f = (a: Int, b:String)=> (a * b.length)/10.0
+      val result=partial1(3, f)
+      assert(result("cheval")===1.8)
+      assert(result("2 lumières")===3.0)
+    }
+  }
+
+  describe("Exercice 4 : currying") {
+    it ("should send in response a function based on currying") {
+      val f = (a: Int, b: Int)=> a+b
+      val g = curry(f)
+      val g_1 = g(1)
+      assert(f(3, 1)===g_1(3) )
+    }
+  }
+
+  describe("Exercice 5 : uncurrying") {
+    it ("should send in response an uncurried function") {
+      val f = (a: Int) => (b: Int) => a+b
+      val f_1 = f(1)
+      val g = uncurry(f)
+      assert(g(3, 1)===f_1(3) )
+    }
+    it ("should send in response an uncurried function with a multiply") {
+      val f = (a: Int) => (b: Int) => a*b
+      val f_4 = f(4)
+      val g = uncurry(f)
+      assert(g(3, 4)===f_4(3) )
+    }
+  }
+
+}
